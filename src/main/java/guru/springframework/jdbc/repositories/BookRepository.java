0f5f044a38571f190b44d83923a3cab4d47930c8
nyguerrillagirl <lorraine.figueroa@brainycode.com>
2023-10-01 10:54:41 -0400

    Completed Null Handling.

--- src/main/java/guru/springframework/jdbc/repositories/BookRepository.java
+++ src/main/java/guru/springframework/jdbc/repositories/BookRepository.java
@@ -2,10 +2,16 @@ package guru.springframework.jdbc.repositories;
 
 import guru.springframework.jdbc.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.Nullable;
 
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     Book findBookByIsbn(String isbn);
     Optional<Book> findBookByTitle(String title);
+
+    Book readByTitle(String title);
+
+    @Nullable
+    Book getByTitle(@Nullable String title);
 }
