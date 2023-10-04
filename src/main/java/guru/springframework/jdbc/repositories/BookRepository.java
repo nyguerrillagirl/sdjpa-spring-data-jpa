cd72de4851d4942655d3bf4e244d29f929e48bf2
nyguerrillagirl <lorraine.figueroa@brainycode.com>
2023-10-03 20:01:12 -0400

    Completed BookRepository method that utilizes Future and @Async.

--- src/main/java/guru/springframework/jdbc/repositories/BookRepository.java
+++ src/main/java/guru/springframework/jdbc/repositories/BookRepository.java
@@ -3,8 +3,10 @@ package guru.springframework.jdbc.repositories;
 import guru.springframework.jdbc.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.lang.Nullable;
+import org.springframework.scheduling.annotation.Async;
 
 import java.util.Optional;
+import java.util.concurrent.Future;
 import java.util.stream.Stream;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
@@ -16,7 +18,8 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     @Nullable
     Book getByTitle(@Nullable String title);
 
-    Stream<Book> findAllByTitleNotNull(
+    Stream<Book> findAllByTitleNotNull();
 
-    );
+    @Async
+    Future<Book> queryByTitle(String title);
 }
